6a02c640a7a0a233ac36a19a1c5f320b89fe527a
kazuki-kumon <kmn13172@gmail.com>
2020-08-06 16:03:41 +0900

    ScriptTestプロジェクト　コミット
    Test.cs、Test2.csにそれぞれの課題を作成しGameObjectコンポーネントにアタッチしました。

---
+++ Assets/Test.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Test : MonoBehaviour
+{
+
+    // Use this for initialization
+    void Start()
+    {
+        int[] array = { 1, 2, 3, 4, 5 };
+        for (int i = 0; i < 5; i++)
+        {
+            Debug.Log(array[i]);
+        }
+
+        for (int n = 4; n > -1; n--)
+        {
+            Debug.Log(array[n]);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
---
+++ Assets/Test2.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Boss
+{
+    private int mp = 53;
+    public void Magic()
+    {
+        if (mp >= 5)
+        {
+            this.mp = mp - 5;
+            Debug.Log("魔法攻撃をした。残りMPは" + mp);
+
+        }
+        else
+        {
+            Debug.Log("MPが足りないため魔法が使えない。");
+        }
+    }
+}
+
+public class Test2 : MonoBehaviour
+{
+
+    void Start()
+    {
+        Boss lastboss = new Boss();
+        for (int i = 0; i <= 10; i++)
+        {
+            lastboss.Magic();
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
